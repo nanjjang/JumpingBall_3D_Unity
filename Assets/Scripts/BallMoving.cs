f316afde93a63bf7aa4c11cfc9341addd9b03a49
nanjjang <loveyjs0420@gmail.com>
2024-03-22 01:33:04 +0900

    ..

--- Assets/Scripts/BallMoving.cs
+++ Assets/Scripts/BallMoving.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class BallMoving : MonoBehaviour
 {
-    float h;
-    float v;
+    float horizontalInput;
+    float verticalInput;
     Rigidbody rb;
     float mouseX = 0.0f;
     public float jumpingPower = 250.0f;
@@ -23,20 +23,15 @@ public class BallMoving : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        h = Input.GetAxisRaw("Horizontal");
-        v = Input.GetAxisRaw("Vertical");
-        
-        
         if (Input.GetButtonDown("Jump") && !jumping)
         {
             rb.AddForce(Vector3.up * 250);
             jumping = true;
         }
         
-
         Vector3 forwardDirection=transform.forward;
         forwardDirection.y = 0;
-        move = new Vector3(h, 0, v);
+        move = new Vector3(horizontal(), 0, vertical());
         move = Quaternion.LookRotation(forwardDirection) * move;
         transform.position += move * speed * Time.deltaTime;
         if (Input.GetKey(KeyCode.LeftShift))
@@ -47,15 +42,28 @@ public class BallMoving : MonoBehaviour
         mouseX += Input.GetAxis("Mouse X") * senstivity;
 
         Vector3 angle = new Vector3(0, mouseX, 0);
-        transform.rotation=Quaternion.Euler(angle);
-
-
+        Angle(angle);
     }
 
-    private void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Floor")){
             jumping = false;
         }
+    }
+    
+    float horizontal()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        return horizontalInput;
+    }
+    float vertical()
+    {
+        verticalInput = Input.GetAxisRaw("Vertical");
+        return verticalInput;
+    }
+    void Angle(Vector3 angle)
+    {
+        transform.rotation = Quaternion.Euler(angle);
     }
 }
