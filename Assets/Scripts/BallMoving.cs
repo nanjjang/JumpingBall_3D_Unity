56e512b76fea092d63bd5278bd100c84fa1908a9
nanjjang <159261384+nanjjang@users.noreply.github.com>
2024-03-23 14:18:56 +0900

    아이템 전까지

--- Assets/Scripts/BallMoving.cs
+++ Assets/Scripts/BallMoving.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class BallMoving : MonoBehaviour
 {
@@ -8,51 +9,67 @@ public class BallMoving : MonoBehaviour
     float verticalInput;
     Rigidbody rb;
     float mouseX = 0.0f;
-    public float jumpingPower = 250.0f;
     public float speed;
     public float superSpeed;
     Vector3 move;
     public float senstivity = 5.0f;
     bool jumping = false;
+    public Transform SpawnPoint;
+    public GameObject Player;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        Respawn();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Jump") && !jumping)
+        if (Input.GetButtonDown("Jump"))
         {
-            rb.AddForce(Vector3.up * 250);
-            jumping = true;
+            if (jumping == false)
+            {
+                rb.AddForce(Vector3.up * 4000);
+                jumping = true;
+                Debug.Log(jumping);
+            }
         }
-        
-        Vector3 forwardDirection=transform.forward;
-        forwardDirection.y = 0;
-        move = new Vector3(horizontal(), 0, vertical());
-        move = Quaternion.LookRotation(forwardDirection) * move;
-        transform.position += move * speed * Time.deltaTime;
+
+        Vector3 moveDirection = transform.forward * vertical() + transform.right * horizontal();
+        moveDirection.Normalize(); // 이동 방향을 정규화
+        rb.AddForce(moveDirection * speed, ForceMode.Impulse);
         
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            transform.position += move * superSpeed * Time.deltaTime;
+            rb.AddForce(moveDirection * superSpeed, ForceMode.Impulse);
         }
 
         mouseX += Input.GetAxis("Mouse X") * senstivity;
 
         Vector3 angle = new Vector3(0, mouseX, 0);
         Angle(angle);
+
+        if (transform.position.y < -10)
+        {
+            Respawn();
+        }
     }
 
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Floor")){
             jumping = false;
+            Debug.Log(jumping);
+        }
+        
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            rb.AddForce(Vector3.back * 900, ForceMode.Impulse);
         }
     }
-    
+
+
     float horizontal()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
@@ -66,5 +83,9 @@ public class BallMoving : MonoBehaviour
     void Angle(Vector3 angle)
     {
         transform.rotation = Quaternion.Euler(angle);
+    }
+    void Respawn()
+    {
+        Player.transform.position = SpawnPoint.position;
     }
 }
